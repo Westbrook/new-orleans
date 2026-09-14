8933f38300c30e412db2babe55c5682f1d97ab6e
Westbrook Johnson <westbrook.johnson@gmail.com>
2026-09-14 18:29:19 -0400

    Publish Places sorting controls

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Generated from every local asset. No third-party requests are cached. */
 const SCOPE = new URL(self.registration.scope).pathname;
 const PREFIX = 'nola-guide:' + SCOPE + ':';
-const CACHE = PREFIX + "nola-a43331670bf1";
+const CACHE = PREFIX + "nola-fb7dddbc40ee";
 const FILES = ["./","./app.js","./assets/icon.svg","./assets/jackson-square.jpg","./components.css","./data/map.json","./data/original-list.txt","./data/venues.json","./guide-utils.js","./index.html","./itinerary.js","./manifest.webmanifest","./styles.css","./vendor/EN-REVE-LICENSE","./vendor/LIT-LICENSE","./vendor/NOTICE.txt","./vendor/SIGNAL-POLYFILL-LICENSE","./vendor/SIGNAL-UTILS-LICENSE","./vendor/en-reve.js","./vendor/tokens.css","./offline-manifest.json"];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES.map(file => new URL(file,self.registration.scope).href)))));
 self.addEventListener('activate', event => event.waitUntil((async()=>{for(const key of await caches.keys())if(key.startsWith(PREFIX)&&key!==CACHE)await caches.delete(key);await self.clients.claim();})()));
